50168a71220d6d5e2fc36a1b110d629aaa0d13e1
George <yurko25@outlook.com>
2019-12-30 22:18:00 +0200

    Initial commit

---
+++ quiz/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from './hoc/Layout.jsx';
+
+import Quiz from './containers/Quiz/Quiz.jsx';
+function App() {
+  return (
+    <Layout>
+    	<Quiz />
+    </Layout>
+  );
+}
+
+export default App;
---
+++ quiz/src/components/ActiveQuiz/ActiveQuiz.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './ActiveQuiz.module.scss';
+
+export default () => {
+	return (
+		<div>Active Quiz</div>
+	);
+}
